7ca64d5de9ed35a0d69650bd2158a79ea5f6321b
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2016-03-28 01:08:05 +0100

    Added simple Puzzle and Cell classes that comes in the form of <table> and <td> elements

--- app/main.js
+++ app/main.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -12,6 +12,18 @@ Puzzle Mum 2016 - Puzzle games for my mother
 ********************************************************************************
  */
 
+var CELL_EMPTY = '';
+var CELL_FILLED = 'filled';
+var CELL_CROSSED = 'crossed';
+var WHITE = '#ffffff';
+var GREY = '#666666';
+var PINK = '#ff9999';
+var GREEN = '#66cc99';
+var BLUE = '#66ccff';
+var ORANGE = '#ffcc33';
+var RENDER_SMALL = 0;
+var RENDER_FULL = 1;
+
 /*  Primary App Class
  */
 //==============================================================================
@@ -21,30 +33,133 @@ var App = function () {
     _classCallCheck(this, App);
 
     //--------------------------------
-    App.FRAMES_PER_SECOND = 1;
+    //App.FRAMES_PER_SECOND = 1;
     //--------------------------------
 
     //--------------------------------
-    this.container = document.getElementById("app");
-    this.console = document.getElementById("console");
-    //--------------------------------
-
-    //--------------------------------
-    this.runCycle = setInterval(this.run.bind(this), 1000 / App.FRAMES_PER_SECOND);
+    this.init();
+    //this.runCycle = setInterval(this.run.bind(this), 1000 / App.FRAMES_PER_SECOND);
     //--------------------------------
   }
 
   //----------------------------------------------------------------
 
   _createClass(App, [{
-    key: "run",
-    value: function run() {
-      this.console.innerHTML += '1';
+    key: 'init',
+    value: function init() {
+      this.container = document.getElementById('app');
+      this.console = document.getElementById('console');
+
+      this.puzzles = [new Puzzle(this, [[WHITE, WHITE, WHITE, BLUE, BLUE, BLUE, WHITE, BLUE, BLUE, BLUE], [WHITE, BLUE, BLUE, WHITE, WHITE, WHITE, BLUE, WHITE, WHITE, WHITE]])];
+
+      this.container.appendChild(this.puzzles[0].html);
+
+      alert(this.console);
     }
+
+    //----------------------------------------------------------------
+
+  }, {
+    key: 'run',
+    value: function run() {}
   }]);
 
   return App;
 }();
+//==============================================================================
+
+/*  Nonogram Puzzle Class
+ */
+//==============================================================================
+
+
+var Puzzle = function () {
+  function Puzzle(app, correctAnswer) {
+    _classCallCheck(this, Puzzle);
+
+    this.app = app;
+    this.html = document.createElement("table");
+    this.html.className = "puzzle";
+
+    this.cells = [];
+    this.colHeaderHtml = [];
+    this.rowHeaderHtml = [];
+
+    //Construct the cell array
+    //--------------------------------
+    for (var row = 0; row < correctAnswer.length; row++) {
+      var newRow = [];
+      for (var col = 0; col < correctAnswer[row].length; col++) {
+        var newCell = new Cell(this, correctAnswer[row][col], col, row);
+        newRow.push(newCell);
+      }
+      this.cells.push(newRow);
+    }
+    console.log(this.cells);
+    //--------------------------------
+
+    //Create the HTML renders
+    //--------------------------------
+    if (this.cells.length >= 1) {
+      var _newRow = document.createElement("tr");
+      var newHeaderCell = document.createElement("td"); //Empty cell
+      newHeaderCell.className = "non-header";
+      _newRow.appendChild(newHeaderCell);
+      for (var _col = 0; _col < this.cells[0].length; _col++) {
+        var _newHeaderCell = document.createElement("td");
+        _newHeaderCell.className = "col-header";
+        _newRow.appendChild(_newHeaderCell);
+        this.colHeaderHtml.push(_newHeaderCell);
+      }
+      this.html.appendChild(_newRow);
+    }
+
+    for (var _row = 0; _row < this.cells.length; _row++) {
+      var _newRow2 = document.createElement("tr");
+      var _newHeaderCell2 = document.createElement("td");
+      _newHeaderCell2.className = "row-header";
+      _newRow2.appendChild(_newHeaderCell2);
+      this.rowHeaderHtml.push(_newHeaderCell2);
+
+      for (var _col2 = 0; _col2 < this.cells[_row].length; _col2++) {
+        _newRow2.appendChild(this.cells[_row][_col2].html);
+      }
+
+      this.html.appendChild(_newRow2);
+    }
+    //--------------------------------
+  }
+
+  _createClass(Puzzle, [{
+    key: 'verifyAnswers',
+    value: function verifyAnswers() {}
+  }]);
+
+  return Puzzle;
+}();
+//==============================================================================
+
+/*  Puzzle Cell
+ */
+//==============================================================================
+
+
+var Cell = function Cell(puzzle, value, x, y) {
+  var _this = this;
+
+  _classCallCheck(this, Cell);
+
+  this.puzzle = puzzle;
+  this.correctAnswer = value;
+  this.playerAnswer = CELL_EMPTY;
+  this.x = x;
+  this.y = y;
+  this.html = document.createElement("td");
+  this.html.className = "cell";
+  this.html.onclick = function () {
+    alert(_this.x + "," + _this.y + ":" + _this.correctAnswer);
+  };
+};
 //==============================================================================
 
 /*  Initialisations
--- src/main.js
+++ src/main.js
@@ -6,6 +6,18 @@ Puzzle Mum 2016 - Puzzle games for my mother
 ********************************************************************************
  */
 
+const CELL_EMPTY = '';
+const CELL_FILLED = 'filled';
+const CELL_CROSSED = 'crossed';
+const WHITE = '#ffffff';
+const GREY = '#666666';
+const PINK = '#ff9999';
+const GREEN = '#66cc99';
+const BLUE = '#66ccff';
+const ORANGE = '#ffcc33';
+const RENDER_SMALL = 0;
+const RENDER_FULL = 1;
+ 
 /*  Primary App Class
  */
 //==============================================================================
@@ -13,27 +25,124 @@ class App {
     
   constructor() {
     //--------------------------------
-    App.FRAMES_PER_SECOND = 1;
-    //--------------------------------
-
-    //--------------------------------
-    this.container = document.getElementById("app");
-    this.console = document.getElementById("console");
+    //App.FRAMES_PER_SECOND = 1;
     //--------------------------------
     
     //--------------------------------
-    this.runCycle = setInterval(this.run.bind(this), 1000 / App.FRAMES_PER_SECOND);
+    this.init();
+    //this.runCycle = setInterval(this.run.bind(this), 1000 / App.FRAMES_PER_SECOND);
     //--------------------------------
   }
   
   //----------------------------------------------------------------
   
+  init() {
+    this.container = document.getElementById('app');
+    this.console = document.getElementById('console');
+    
+    this.puzzles = [
+      new Puzzle(this, [
+        [WHITE, WHITE, WHITE, BLUE,  BLUE,  BLUE,  WHITE, BLUE,  BLUE,  BLUE ],
+        [WHITE, BLUE,  BLUE,  WHITE, WHITE, WHITE, BLUE,  WHITE, WHITE, WHITE],
+      ])
+    ];
+    
+    this.container.appendChild(this.puzzles[0].html);
+    
+    alert(this.console);
+  }
+  
+  //----------------------------------------------------------------
+  
   run() {
-    this.console.innerHTML += '1';
   }
 
 }
 //==============================================================================
+
+/*  Nonogram Puzzle Class
+ */
+//==============================================================================
+class Puzzle {
+  constructor(app, correctAnswer) {
+    this.app = app;
+    this.html = document.createElement("table");
+    this.html.className = "puzzle";
+    
+    this.cells = [];
+    this.colHeaderHtml = [];
+    this.rowHeaderHtml = [];
+    
+    //Construct the cell array
+    //--------------------------------
+    for (let row = 0; row < correctAnswer.length; row++) {
+      let newRow = [];
+      for (let col = 0; col < correctAnswer[row].length; col++) {
+        let newCell = new Cell(this, correctAnswer[row][col], col, row);
+        newRow.push(newCell);
+      }
+      this.cells.push(newRow);
+    }
+    console.log(this.cells);
+    //--------------------------------
+    
+    //Create the HTML renders
+    //--------------------------------
+    if (this.cells.length >= 1) {
+      let newRow = document.createElement("tr");
+      let newHeaderCell = document.createElement("td");  //Empty cell
+      newHeaderCell.className = "non-header";
+      newRow.appendChild(newHeaderCell);
+      for (let col = 0; col < this.cells[0].length; col++) {
+        let newHeaderCell = document.createElement("td");
+        newHeaderCell.className = "col-header";
+        newRow.appendChild(newHeaderCell);
+        this.colHeaderHtml.push(newHeaderCell);
+      }
+      this.html.appendChild(newRow);
+    }
+    
+    for (let row = 0; row < this.cells.length; row++) {
+      let newRow = document.createElement("tr");
+      let newHeaderCell = document.createElement("td");
+      newHeaderCell.className = "row-header";
+      newRow.appendChild(newHeaderCell);
+      this.rowHeaderHtml.push(newHeaderCell);
+      
+      for (let col = 0; col < this.cells[row].length; col++) {
+        newRow.appendChild(this.cells[row][col].html);
+      }
+      
+      this.html.appendChild(newRow);
+    }
+    //--------------------------------
+  }
+  
+  verifyAnswers() {
+    
+  }
+}
+//==============================================================================
+
+/*  Puzzle Cell
+ */
+//==============================================================================
+class Cell {
+  constructor(puzzle, value, x, y) {
+    this.puzzle = puzzle;
+    this.correctAnswer = value;
+    this.playerAnswer = CELL_EMPTY;
+    this.x = x;
+    this.y = y;
+    this.html = document.createElement("td");
+    this.html.className = "cell";
+    this.html.onclick = () => {
+      alert(this.x + "," + this.y + ":" + this.correctAnswer);
+    };
+  }
+}
+//==============================================================================
+
 
 /*  Initialisations
  */
